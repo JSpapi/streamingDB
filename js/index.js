735da48324f54f80cf8617db32cc1ee0955cea33
axror <axror.kurban@gmail.com>
2022-10-24 16:40:26 +0500

    finished navbar and preloader

---
+++ js/index.js
@@ -0,0 +1,75 @@
+const disableAllBtns = () => {
+  const allBtns = document.querySelectorAll("button");
+  allBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => e.preventDefault());
+  });
+};
+
+// !navbar burger function
+const activeAdaptiveNavbar = () => {
+  const NavbarOpenBtn = document.querySelector(".navbar-burger");
+  const NavbarCloseBtn = document.querySelector(".adaptive-header__close");
+  const adaptiveNavbar = document.querySelector(".adaptive-navbar");
+  const navbar = NavbarOpenBtn.closest(".navbar");
+
+  NavbarOpenBtn.addEventListener("click", () => {
+    adaptiveNavbar.classList.add("active");
+    navbar.style.display = "none";
+  });
+
+  NavbarCloseBtn.addEventListener("click", () => {
+    adaptiveNavbar.classList.remove("active");
+    navbar.style.display = "flex";
+  });
+};
+
+// !navbar burger functions end
+
+// todo // !navbar search function
+const activeAdaptiveSearchbar = () => {
+  const SearchbarBtn = document.querySelector(".navbar-search__btn");
+  const adaptiveSearchbar = document.querySelector(".adaptive-searchbar");
+  const NavbarCloseBtn = document.querySelectorAll(".adaptive-header__close");
+  const navbar = SearchbarBtn.closest(".navbar");
+
+  SearchbarBtn.addEventListener("click", () => {
+    if (screen.width <= 1024) {
+      adaptiveSearchbar.classList.add("active");
+      navbar.style.display = "none";
+    }
+  });
+
+  NavbarCloseBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      adaptiveSearchbar.classList.remove("active");
+      navbar.style.display = "flex";
+    });
+  });
+};
+
+// todo // !navbar search function end
+
+// !PRELOADER
+window.addEventListener('load', ()=> {
+  preloaderFn();
+});
+
+const preloaderFn = ()=> {
+  const preloader = document.querySelector('.preloader');
+  const body = preloader.closest('body');
+  body.removeAttribute('id');
+  preloader.classList.add('disappear');
+}
+
+
+// !PRELOADER END
+
+const triggerAllFunctions = () => {
+  activeAdaptiveNavbar();
+
+  activeAdaptiveSearchbar();
+
+  disableAllBtns();
+};
+
+triggerAllFunctions();
