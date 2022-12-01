c56c8526bec54ed982eeb4368ef0e23d747244e1
JSpapi <104095661+JSpapi@users.noreply.github.com>
2022-12-01 12:25:40 +0500

    add some new features

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,31 +2,31 @@ export const newCollections = () => {
   const newCollectionsData = [
     {
       id: 1,
-		time: '2 недели назад',
+      time: "2 недели назад",
       img: "./images/films/sidebarImg-1.png",
       description: "Задача организации, в особенности же новая формировании.",
     },
     {
       id: 2,
-		time: '3 недели назад',
+      time: "3 недели назад",
       img: "./images/films/sidebarImg-2.png",
       description: "Задача организации, в особенности же новая формировании.",
     },
     {
       id: 3,
-		time: '1 неделю назад',
+      time: "1 неделю назад",
       img: "./images/films/sidebarImg-3.png",
       description: "Задача организации, в особенности же новая формировании.",
     },
     {
       id: 4,
-		time: '2 дня назад',
+      time: "2 дня назад",
       img: "./images/films/sidebarImg-4.png",
       description: "Задача организации, в особенности же новая формировании.",
     },
     {
       id: 5,
-		time: '1 месяц назад',
+      time: "1 месяц назад",
       img: "./images/films/sidebarImg-5.png",
       description: "Задача организации, в особенности же новая формировании.",
     },
@@ -47,7 +47,7 @@ const createNewCollectionMovies = (
     .map(({ img, description, id, time }) => {
       return `
 			<div class="new__collection-movie">
-				<a href="./pages/movie.html" class="new__collection-img">
+				<a href="#!" class="new__collection-img">
 					<img src="${img}" alt="sidebar-${id}" />
 				</a>
 				<div class="new__collection-content">
@@ -56,7 +56,7 @@ const createNewCollectionMovies = (
 						${time}
 					</p>
 
-					<a class="new__collection-description" href="./pages/movie.html">
+					<a class="new__collection-description" href="#!">
 						${description}
 					</a>
 				</div>
@@ -64,15 +64,14 @@ const createNewCollectionMovies = (
 		`;
     })
     .join("");
-    // !CHECKING IF THERE IS THIS CLASS, UNLESS RETURN 
-    if(newCollectionParrent){
-      if(newCollectionParrent){
-        newCollectionParrent.innerHTML = createNewCollection;
-      }else{
-        return;
-      }
-    }else{
+  // !CHECKING IF THERE IS THIS CLASS, UNLESS RETURN
+  if (newCollectionParrent) {
+    if (newCollectionParrent) {
+      newCollectionParrent.innerHTML = createNewCollection;
+    } else {
       return;
     }
-
+  } else {
+    return;
+  }
 };
